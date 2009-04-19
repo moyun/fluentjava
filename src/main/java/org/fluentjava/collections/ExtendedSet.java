d555f64a2091601f7e5ef07af0b72614520b2075
carlos.acp <carlos.acp@b34f4df6-d545-0410-9116-b5cd15111316>
2009-04-19 20:10:45 +0000

    Comments at class

--- src/main/java/org/fluentjava/collections/ExtendedSet.java
+++ src/main/java/org/fluentjava/collections/ExtendedSet.java
@@ -6,10 +6,28 @@ import java.util.HashSet;
 import org.fluentjava.iterators.ExtendedIterator;
 import org.fluentjava.iterators.ExtendedIteratorAdapter;
 
+/**
+ * Standard implementation of FluentSet.
+ * @param <E>
+ * Type of elements
+ */
 public class ExtendedSet<E> extends HashSet<E> implements FluentSet<E> {
-
 	private static final long serialVersionUID = 1L;
 
+	/*
+	 * Factory Methods
+	 */
+	public static <E> ExtendedSet<E> set(E...element) {
+		return new ExtendedSet<E>(element);
+	}
+	
+	public static <E> ExtendedSet<E> set(Iterable<E> iterable) {
+		return new ExtendedSet<E>(iterable);
+	}
+
+	/*
+	 * Constructors
+	 */
 	public ExtendedSet() { }
 	
 	public ExtendedSet(E...elements) {
@@ -20,31 +38,27 @@ public class ExtendedSet<E> extends HashSet<E> implements FluentSet<E> {
 		insert(element);
 	}
 
-	public static <E> ExtendedSet<E> set(E...element) {
-		return new ExtendedSet<E>(element);
+	/*
+	 * Public Methods
+	 */
+	
+	@Override
+	public ExtendedIterator<E> iterator() {
+	return new ExtendedIteratorAdapter<E>(super.iterator());
 	}
 
-	/* (non-Javadoc)
-	 * @see org.fluentjava.collections.FluentSet#insert(E)
-	 */
-	public FluentSet<E> insert(E... element) {
-		for (E e : element) {
-			add(e);
-		}
-		return this;
-	}
-
-	/* (non-Javadoc)
-	 * @see org.fluentjava.collections.FluentSet#insert(E)
-	 */
 	public FluentSet<E> insert(E element) {
 		add(element);
 		return this;
 	}
 	
-	/* (non-Javadoc)
-	 * @see org.fluentjava.collections.FluentSet#insert(java.lang.Iterable)
-	 */
+	public FluentSet<E> insert(E... set) {
+		for (E e : set) {
+			add(e);
+		}
+		return this;
+	}
+	
 	public FluentSet<E> insert(Iterable<E> iterable) {
 		for (E e : iterable) {
 			add(e);
@@ -52,27 +66,15 @@ public class ExtendedSet<E> extends HashSet<E> implements FluentSet<E> {
 		return this;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.fluentjava.collections.FluentSet#delete(E)
-	 */
 	public FluentSet<E> delete(E...elements) {
 		removeAll(set(elements));
 		return this;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.fluentjava.collections.FluentSet#array(java.lang.Class)
-	 */
 	@SuppressWarnings("unchecked")
 	public <T> T[] array(Class<T> clazz) {
 		return 	toArray((T[]) Array.newInstance(clazz, size()));
 	}
 	
-	/* (non-Javadoc)
-	 * @see org.fluentjava.collections.FluentSet#iterator()
-	 */
-	@Override
-	public ExtendedIterator<E> iterator() {
-	return new ExtendedIteratorAdapter<E>(super.iterator());
-	}
+	
 }
