1e44203b6a54a42433af440c7f6853dde6d11d34
carlos.acp <carlos.acp@b34f4df6-d545-0410-9116-b5cd15111316>
2009-04-19 17:32:08 +0000

    Now we have FluentSet! Mmm...  maybe we should rename the class...

---
+++ trunk/src/test/java/org/fluentjava/collections/FluentSetTest.java
@@ -0,0 +1,64 @@
+package org.fluentjava.collections;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
+public class FluentSetTest {
+
+	@Test
+	public void testCreatingSequence() throws Exception {
+		FluentSet<Integer> set = new FluentSet<Integer>(1, 2, 2, 3);
+		assertEquals(asSet(1, 2, 2, 3), set);
+	}
+	
+	@Test
+	public void testFactoryMethods() throws Exception {
+		assertEquals(asSet(1, 2, 3, 3), FluentSet.set(1, 2, 3, 3));
+	}
+
+	@Test
+	public void testFluentStyle() throws Exception {
+		FluentSet<Integer> list = new FluentSet<Integer>();
+		list.insert(1)
+			.insert(2, 2, 3)
+			.insert(4, 4, 4, 5, 5)
+			.insert(asSet(6, 7, 8))
+			.delete(4, 5, 6, 7, 8)
+			.delete(110)
+			.add(3);
+		assertEquals(asSet(1, 2, 3), list);
+	}
+	
+	@Test
+	public void testCheckedToArrayIsUsefullForVariableArgumentsMethods() throws Exception {
+		FluentSet<Number> set = new FluentSet<Number>(2, 4, 6);
+		assertEquals(asSet(1, 2, 3), half(set.array(Integer.class)));
+	}
+	
+	private Set<Integer> half(Integer...array) {
+		Set<Integer> set = new HashSet<Integer>();
+		for (Integer integer : array) {
+			set.add(integer / 2);
+		}
+		return set;
+	}
+
+	@Test(expected = Exception.class)
+	public void testCannotConvertEverything() throws Exception {
+		FluentList<Integer> list = Sequence.list(2, 4, 6);
+		list.array(String.class);
+	}
+	
+	private <E> Set<E> asSet(E...element) {
+		Set<E> set = new HashSet<E>();
+		for (E e : element) {
+			set.add(e);
+		}
+		return set;
+	}
+	
+}
