c1b5ed718e472f389a34f20ce3aa02acbbd0aad8
carlos.acp <carlos.acp@b34f4df6-d545-0410-9116-b5cd15111316>
2009-04-19 20:24:25 +0000

    Javadoc

--- trunk/src/main/java/org/fluentjava/collections/FluentSet.java
+++ trunk/src/main/java/org/fluentjava/collections/FluentSet.java
@@ -6,19 +6,68 @@ import org.fluentjava.iterators.ExtendedIterable;
 
 
 /**
+ * Set that has type safe toArray, and has a fluent interface on methods. Also, has more
+ * convenient methods to include and remove elements.
  * 
- * TODO: add javadoc.
  * @param <E>
+ * Type of elements
  */
 public interface FluentSet<E> extends Set<E>, ExtendedIterable<E> {
 
+	/**
+	 * Simliar to the method toArray, but recieves no args and is type safe. Also, the
+	 * returned array is just a copy. Note that the class is necessary, as Gererics by
+	 * themselves cannot solve this problem alone. For more info:
+	 * http://www.ibm.com/developerworks/java/library/j-jtp01255.html
+	 * 
+	 * @return A copy of the set as an array.
+	 */
+	<T> T[] array(Class<T> clazz);
+	
+	/**
+	 * Fluent method (returns self). Adds all elements on the list.
+	 * 
+	 * @param set
+	 * The elements to be inserted.
+	 * @return self
+	 */
+	FluentSet<E> insert(E... set);
+	
+	/**
+	 * Fluent method (returns self). Add one element
+	 * 
+	 * @param element
+	 * The element to be inserted.
+	 * @return
+	 */
 	FluentSet<E> insert(E element);
 
-	FluentSet<E> insert(E... element);
-
+	/**
+	 * Fluent method (returns self). Adds all elements of the iterable.
+	 * 
+	 * @param iterable
+	 * The elements returned by it are inserted.
+	 * @return self
+	 */
 	FluentSet<E> insert(Iterable<E> iterable);
 
-	FluentSet<E> delete(E...elements);
+	/**
+	 * Fluent method (returns self). Removes all elements on the set.
+	 * 
+	 * @param set
+	 * The elements to be removed.
+	 * @return self
+	 */
+	FluentSet<E> delete(E...set);
 
-	<T> T[] array(Class<T> clazz);
+	/**
+	 * Fluent method (returns self). Removes all elements on the iterable.
+	 * 
+	 * @param iterable
+	 * The elements returned by it are removed.
+	 * @return self
+	 */
+	FluentSet<E> delete(Iterable<E> iterable);
+
+
 }
