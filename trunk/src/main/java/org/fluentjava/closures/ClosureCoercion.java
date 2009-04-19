3e4918bc1d13084670f6fe7158b2de4728713e8f
danrbr <danrbr@b34f4df6-d545-0410-9116-b5cd15111316>
2009-04-19 23:14:45 +0000

    More into closure Coercion.

--- trunk/src/main/java/org/fluentjava/closures/ClosureCoercion.java
+++ trunk/src/main/java/org/fluentjava/closures/ClosureCoercion.java
@@ -1,22 +1,62 @@
 package org.fluentjava.closures;
 
+import java.lang.reflect.Method;
+import java.util.Comparator;
+
+import org.fluentjava.collections.FluentList;
+import org.fluentjava.collections.Sequence;
+import org.fluentjava.reflection.RuntimeReflectionException;
 
 /**
  * Converts Objects do Closures, or throws ClosureCoercionException if can't.
  */
 public class ClosureCoercion {
+	/*
+	 * Public Class Methods
+	 */
 	public static Closure toClosure(Object closure) {
-		if (!(closure instanceof Closure)) {
-			throw new ClosureCoercionException("Argument does not coerce to closure: " + closure);
+		if (closure instanceof Closure) {
+			Closure function = (Closure) closure;
+			return function;
 		}
-		Closure function = (Closure) closure;
-		return function;
+		Method method = getSingleAbstractMethod(closure);
+		if (method != null) {
+			return new ClosureOfAMethod(closure, method);
+		}
+		if (closure instanceof Comparator) {
+			return comparatorToClosure(closure);
+		}
+		throw new ClosureCoercionException("Argument does not coerce to closure: " + closure);
+		
 	}
-	
 	public static Predicate toPredicate(Object predicate) {
 		if (predicate instanceof Predicate) {
 			return (Predicate) predicate;
 		}
 		return new PredicateAdapter(toClosure(predicate));
+	}
+	
+	/*
+	 * Private Class Methods
+	 */
+	private static Method getSingleAbstractMethod(Object closure) {
+		FluentList<Class<?>> possibleInterfaces = new Sequence<Class<?>>();
+		for (Class<?> inter : closure.getClass().getInterfaces()) {
+			if (inter.getMethods().length == 1) {
+				possibleInterfaces.add(inter);
+			}
+		}
+		if (possibleInterfaces.size() == 1) {
+			return possibleInterfaces.get(0).getMethods()[0];
+		}
+		return null;
+	}
+	
+	private static Closure comparatorToClosure(Object closure) {
+		try {
+			return new ClosureOfAMethod(closure, Comparator.class.getMethod("compare", Object.class, Object.class));
+		} catch (NoSuchMethodException e) {
+			throw new RuntimeReflectionException(e);
+		}
 	}
 }
--- trunk/src/test/java/org/fluentjava/collections/SequenceTest.java
+++ trunk/src/test/java/org/fluentjava/collections/SequenceTest.java
@@ -1,12 +1,10 @@
 package org.fluentjava.collections;
 import static java.util.Arrays.asList;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.fluentjava.closures.Closure;
-import org.fluentjava.closures.Predicate;
 import org.junit.Test;
 
 public class SequenceTest {
@@ -56,142 +54,6 @@ public class SequenceTest {
 		assertEquals(asList(10, 2, 3), asListResult);
 		assertEquals(asList(1, 2, 3), list);
 	}
-
-	@Test
-	public void testExistsWithClosures() {
-		FluentList<Integer> list = Sequence.list(1, 2, 3, 4, 5);
-		assertTrue(list.exists(greaterThan(4)));
-	}
-	
-	@Test(expected = EnumeratingException.class)
-	public void testClosureMustReturnBooleanWhenUsingExists() throws Exception {
-		FluentList<Integer> list = Sequence.list(1, 2, 3, 4, 5);
-		Closure badClosure = new Closure() {
-			@Override
-			public Object call(Object... args) throws Exception {
-				return new ArrayList<Object>();
-			}
-		};
-		list.exists(badClosure);
-	}
-	
-	@Test
-	public void testAllSatisfy() throws Exception {
-		FluentList<Integer> list = Sequence.list(5, 6, 7);
-		assertTrue(list.allSatisfy(greaterThan(4)));
-	}
-	
-	@Test
-	public void testAnySatisfy() throws Exception {
-		FluentList<Integer> list = Sequence.list(1, 2, 3);
-		assertFalse(list.anySatisfy(greaterThan(4)));
-	}
-	
-	@Test
-	public void testAnySatisfyWithOneSatifying() throws Exception {
-		FluentList<Integer> list = Sequence.list(1, 2, 3, 30);
-		assertTrue(list.anySatisfy(greaterThan(4)));
-	}
-	
-	@Test
-	public void testNoneSatisfy() throws Exception {
-		FluentList<Integer> list = Sequence.list(1, 2, 3);
-		assertTrue(list.noneSatisfy(greaterThan(4)));
-	}
-	
-	@Test
-	public void testSelect() throws Exception {
-		FluentList<Integer> list = Sequence.list(1, 2, 3, 5, 6, 7);
-		assertEquals(asList(5, 6, 7), list.select(greaterThan(4)));
-	}
-	
-	@Test
-	public void testReject() throws Exception {
-		FluentList<Integer> list = Sequence.list(1, 2, 3, 5, 6, 7);
-		assertEquals(asList(1, 2, 3), list.reject(greaterThan(4)));
-	}
-	
-	@Test
-	public void testDetectNonExistant() throws Exception {
-		FluentList<Integer> list = Sequence.list(1, 2, 3);
-		assertNull(list.detect(greaterThan(4)));
-	}
-	
-	@Test
-	public void testDetect() throws Exception {
-		FluentList<Integer> list = Sequence.list(1, 2, 3, 8, 9, 10);
-		assertEquals(8, list.detect(greaterThan(4)));
-	}
-	
-	
-	@Test
-	public void testDetectIfNone() throws Exception {
-		FluentList<Integer> list = Sequence.list(1, 2, 3);
-		assertEquals(8, list.detectIfNone(greaterThan(4), 8));
-	}
-	
-	@Test
-	public void testCount() throws Exception {
-		FluentList<Integer> list = Sequence.list(1, 2, 3, 5, 6, 7);
-		assertEquals(3, list.count(greaterThan(4)));
-	}
-	
-	@Test
-	public void testName() throws Exception {
-		final FluentList<Integer> acummulated = new Sequence<Integer>();
-		FluentList<Integer> list = Sequence.list(1, 2, 3, 4, 5, 6, 7);
-		list.foreach(new Closure() {
-			@Override
-			public Object call(Object... args) throws Exception {
-				Integer i = first(args);
-				if (i % 2 == 0) {
-					acummulated.add(i);
-				}
-				return null;
-			}
-		});
-		assertEquals(asList(2, 4, 6), acummulated);
-	}
-	
-	@Test
-	public void testMap() throws Exception {
-		FluentList<Integer> list = Sequence.list(1, 2, 3);
-		FluentList<Integer> ret = list.map(new Closure() {
-			@Override
-			public Object call(Object... args) throws Exception {
-				Integer i = first(args);
-				return i * 3;
-			}
-		});
-		assertEquals(asList(3, 6, 9), ret);	
-	}
-	
-	@Test
-	public void testMapToDifferentTypes() throws Exception {
-		FluentList<String> list = Sequence.list("I", "am", "one");
-		FluentList<Integer> ret = list.map(new Closure() {
-			@Override
-			public Object call(Object... args) throws Exception {
-				String i = first(args);
-				return i.length();
-			}
-		});
-		assertEquals(asList(1, 2, 3), ret);	
-	}
-	
-	@Test
-	public void testSortWithClosure() throws Exception {
-		FluentList<String> list = Sequence.list("big", "ones", "always come", "before");
-		FluentList<String> ret = list.sort(new Closure() {
-			@Override
-			public Object call(Object... args) throws Exception {
-				String i = first(args);
-				String j = second(args);
-				return j.length() - i.length();
-			}
-		});
-		assertEquals(asList("always come", "before", "ones", "big"), ret);	
-	}
 	
 	@Test
 	public void testToListCreatesACopy() throws Exception {
@@ -199,51 +61,7 @@ public class SequenceTest {
 		FluentList<Integer> copy = list.toList();
 		assertEquals(copy, list);
 	}
-	
-	@Test
-	public void testSortingWithNaturalOrder() throws Exception {
-		FluentList<Integer> list = Sequence.list(3, 2, 1);
-		assertEquals(asList(1, 2, 3), list.sort());
-	}
-	
-	@Test
-	public void testReduce() throws Exception {
-		FluentList<Integer> list = Sequence.list(1, 2, 3);
-		Integer resultado = list.reduce(sumBlock());
-		assertEquals(6, resultado);
-	}
 
-	@Test
-	public void testReduceReturnsNullIfListIsEmpty() throws Exception {
-		FluentList<Integer> list = new Sequence<Integer>();
-		assertNull(list.reduce(sumBlock()));
-	}
-	
-	@Test
-	public void testReduceWithStartingElement() throws Exception {
-		FluentList<Integer> list = Sequence.list(1, 2, 3);
-		Integer resultado = list.reduce(10, sumBlock());
-		assertEquals(16, resultado);
-	}
-	
-	@Test
-	public void testReducewithEmtpyListAndStartingEelment() throws Exception {
-		FluentList<Integer> list = new Sequence<Integer>();
-		assertEquals(10, list.reduce(10, sumBlock()));
-	}
-
-	private Predicate greaterThan(final int number) {
-		Predicate anyGreaterThan = new Predicate() {
-			@Override
-			public boolean eval(Object... args) throws Exception {
-				Integer i = first(args);
-				return i > number;
-			}
-		};
-		return anyGreaterThan;
-	}
-
-	
 	private List<Integer> half(Integer... array) {
 		ArrayList<Integer> ret = new ArrayList<Integer>();
 		for (Integer integer : array) {
@@ -252,18 +70,6 @@ public class SequenceTest {
 		return ret;
 	}
 	
-	private Closure sumBlock() {
-		Closure sumBlock = new Closure() {
-			@Override
-			public Object call(Object... args) throws Exception {
-				Integer x = first(args);
-				Integer y = second(args);
-				return x + y;
-			}
-		};
-		return sumBlock;
-	}
-	
-	
+		
 	
 }
