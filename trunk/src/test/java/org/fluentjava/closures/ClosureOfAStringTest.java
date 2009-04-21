f5c069c7bac8bc5263713142bc4e2378dce6e8b4
danrbr <danrbr@b34f4df6-d545-0410-9116-b5cd15111316>
2009-04-21 21:39:02 +0000

    Closure of a String now supports Varargs and overloadedmethods differing in args (still not in types)

---
+++ trunk/src/test/java/org/fluentjava/closures/ClosureOfAStringTest.java
@@ -0,0 +1,65 @@
+package org.fluentjava.closures;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ClosureOfAStringTest {
+
+	@Test
+	public void testAClassWithOverloadedMethodsArgwise() throws Exception {
+		OverloadedMethodsArgwise mock = new OverloadedMethodsArgwise();
+		ClosureOfAString closure = new ClosureOfAString("inc");
+		Integer oneArgRet = closure.invoke(mock, 0);
+		assertEquals(1, oneArgRet);
+		
+		Integer twoArgsRet = closure.invoke(mock, 0, 9);
+		assertEquals(9, twoArgsRet);
+		
+	}
+	
+	@Test
+	public void testSimpleVarArgs() throws Exception {
+		SimpleVarArrgs mock = new SimpleVarArrgs();
+		ClosureOfAString closure = new ClosureOfAString("inc");
+		Integer ret = closure.invoke(mock, 0, "one", "two");
+		assertEquals(1, ret);
+	}
+	
+	@Test
+	public void testWithOverloadedVarARgs() throws Exception {
+		OverloadedVarArgs mock = new OverloadedVarArgs();
+		ClosureOfAString closure = new ClosureOfAString("inc");
+		Integer oneArgRet = closure.invoke(mock, 0);
+		assertEquals(1, oneArgRet);
+		
+		Integer twoArgsRet = closure.invoke(mock, 0, 9);
+		assertEquals(9, twoArgsRet);
+	}
+	
+	private static class OverloadedVarArgs {
+		public int inc(Integer i, Integer offset, String... rest) {
+			return i + offset;
+		}
+		
+		public int inc(Integer i, String... rest) {
+			return i + 1;
+		}
+	}
+	
+	private static class SimpleVarArrgs {
+		public int inc(Integer i, String... rest) {
+			return i + 1;
+		}
+	}
+	
+	private static class OverloadedMethodsArgwise {
+		public int inc(Integer i) {
+			return inc(i, 1);
+		}
+		
+		public int inc(Integer i, Integer offset) {
+			return i + offset;
+		}
+	}
+}
