5eb520586bfa80cf0563bdc2109b426c985bf9c4
danrbr <danrbr@b34f4df6-d545-0410-9116-b5cd15111316>
2009-04-21 21:39:02 +0000

    Closure of a String now supports Varargs and overloadedmethods differing in args (still not in types)

--- src/main/java/org/fluentjava/closures/ClosureOfAString.java
+++ src/main/java/org/fluentjava/closures/ClosureOfAString.java
@@ -2,7 +2,10 @@ package org.fluentjava.closures;
 
 import static java.util.Arrays.asList;
 
+import java.lang.reflect.Array;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.List;
 
 /**
  * Wraps around a public method name. The first argument of call must always be a class
@@ -18,19 +21,42 @@ public class ClosureOfAString extends Closure {
 
 	@Override
 	public Object call(Object... args) throws Exception {
-		Object first = first(args);
-		Object[] rest = asList(args).subList(1, args.length).toArray();
-		Method method = findmethod(first);
-		return method.invoke(first, rest);
-	}
-
-	private Method findmethod(Object target) {
+		Object target = first(args);
+		List<Object> restList = asList(args).subList(1, args.length);
 		for (Method method : target.getClass().getMethods()) {
 			if (methodName.equals(method.getName())) {
-				return method;
+				if (method.isVarArgs()) {
+					if (restList.size() >= (method.getParameterTypes().length - 1)) {
+						return invokeVarArgs(target, restList, method);
+					}
+				}
+				else if (method.getParameterTypes().length == restList.size()) {
+					return method.invoke(target, restList.toArray());
+				}
+
 			}
 		}
 		throw new IllegalArgumentException("Method of name " + methodName + " could not be found on "
 				+ target);
 	}
+
+	private Object invokeVarArgs(Object target, List<Object> argList, Method method)
+			throws IllegalAccessException, InvocationTargetException {
+		Class<?>[] argTypes = method.getParameterTypes();
+		Object[] toInvoke = new Object[argTypes.length];
+		int nonVarArgsCount = argTypes.length - 1;
+		for (int i = 0; i < nonVarArgsCount; i++) {
+			toInvoke[i] = argList.get(i);
+		}
+		toInvoke[nonVarArgsCount] = buildVarArgsArray(argList, argTypes, nonVarArgsCount);
+		return method.invoke(target, toInvoke);
+	}
+
+	private Object[] buildVarArgsArray(List<Object> argList, Class<?>[] argTypes, int nonVarArgsCount) {
+		Class<?> typeOfVarArgs = argTypes[nonVarArgsCount];
+		assert (typeOfVarArgs.isArray());
+		return (Object[]) Array.newInstance(typeOfVarArgs.getComponentType(), argList.size()
+				- argList.size());
+	}
+
 }
